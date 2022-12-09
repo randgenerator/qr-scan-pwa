96054fb8ac24b38aabde23e9af7b3a367496d78f
Oqil Ahmad <oqilaxmatov24@gmail.com>
2022-12-09 17:12:40 +0500

    add

--- src/pages/AttendanceList/index.tsx
+++ src/pages/AttendanceList/index.tsx
@@ -190,7 +190,7 @@ const AttendanceList = () => {
         let convertedUTC;
 
         if (attendee.verified_at) {
-          convertedUTC = new Date(attendee.verified_at);
+          convertedUTC = new Date(attendee.verified_at?.toLocaleString({ hour12: false }));
         }
 
         return (
